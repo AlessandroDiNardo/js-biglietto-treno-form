7da741cb72f3ad4d98a3f1a018260a0fdcd661f9
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-08 19:54:02 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -10,29 +10,65 @@ let element = document.getElementById("generate");
 element.addEventListener(`click`, 
 
     function() {
-        let name = document.getElementById("name").value;
+
+        // richiede nome e cognome
+        let nome = document.getElementById("nome").value;
+
+        // richiede km
         let km = document.getElementById("km").value;
+
+        // richiede età
         let age = document.getElementById("age").value;
+
+        // prezzo biglietto
         let prezzo = 0.21 * km;
         console.log(prezzo);
         let tot;
         let sconto;
-        if(age < 18){
+        if(age === "minorenne"){
             sconto = (prezzo / 100) * 20;
             tot = prezzo - sconto;
             console.log(tot);
-        } else if(age > 65){
+            // offerta
+            document.getElementById("offer").innerHTML = `Sconto del 20%`
+        } else if(age === "senior"){
             sconto = (prezzo / 100) * 40;
-            console.log(tot);
             tot = prezzo - sconto;
-        } else{
             console.log(tot);
+            // offerta
+            document.getElementById("offer").innerHTML = `Sconto del 40%`
+        } else{
             tot =  prezzo;
+            console.log(tot);
+            document.getElementById("offer").innerHTML = `Nessuno sconto applicabile`
         }
         document.getElementById("price").innerHTML = `${tot.toFixed(2)}€`;
-    }
-    
+
+        // nome passeggero su ticket
+        document.getElementById("namePassenger").innerHTML = nome;
+
+        // numero carrozza
+        document.getElementById("numCarr").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+        // codice CP
+        document.getElementById("CP").innerHTML = Math.floor(Math.random() * 100000) + 1;
+
+
+        // apparizione secondo biglietto
+        const yourTicket = document.getElementById("boxOutput");
+        yourTicket.style.display = "block";
+
+
+    }    
+
 );                                               
 
-
-//output
+// button reset
+let elementReset = document.getElementById("reset");
+elementReset.addEventListener(`click`,
+    function(){
+        document.getElementById("infoBlock").reset();
+        const yourTicket = document.getElementById("boxOutput");
+        yourTicket.style.display = "none";
+    }
+);
