d272f26e41b0c4a2e447f917595598870600d0d5
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-08 17:26:58 +0100

    problem ciclo if output

--- js/main.js
+++ js/main.js
@@ -6,45 +6,29 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65. */
 
 
-// chiedere nome e cognome
-let name = document.getElementById("name");
-
-console.log(name)
-
-//chiedere numero di km
-let km = document.getElementById("km");
-
-console.log(km);
-
-//chiedere età
-let age = document.getElementById("age");
-console.log(age);
-
-
-// calcolo perzzo biglietto per intero
-const costoBiglietto = 0.21;
-let prezzo = costoBiglietto * km;
-console.log(prezzo);
-
-//applicazione sconto
-let tot;
-let sconto;
-
-// sconto per minorenni
-if(age < 18){
-    sconto = (prezzo / 100) * 20;
-    tot = prezzo - sconto;
-}else if(age > 65){
-    sconto = (prezzo / 100) * 40;
-    tot = prezzo - sconto;
-}else{
-    tot =  prezzo;
-}
-
 let element = document.getElementById("generate");
 element.addEventListener(`click`, 
 
     function() {
+        let name = document.getElementById("name").value;
+        let km = document.getElementById("km").value;
+        let age = document.getElementById("age").value;
+        let prezzo = 0.21 * km;
+        console.log(prezzo);
+        let tot;
+        let sconto;
+        if(age < 18){
+            sconto = (prezzo / 100) * 20;
+            tot = prezzo - sconto;
+            console.log(tot);
+        } else if(age > 65){
+            sconto = (prezzo / 100) * 40;
+            console.log(tot);
+            tot = prezzo - sconto;
+        } else{
+            console.log(tot);
+            tot =  prezzo;
+        }
         document.getElementById("price").innerHTML = `${tot.toFixed(2)}€`;
     }
     
